ebea1250f476279e17959ec9b565d48ef43108d2
Mohamed Ashraf <68484397+mohamedashraf-eng@users.noreply.github.com>
2022-08-01 20:07:20 +0200

    Stringer things

---
+++ Data-Base/libs/Stringer/build/CMakeFiles/Stringer_lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Stringer_lib.dir/src/Stringer.c.obj"
+  "CMakeFiles/Stringer_lib.dir/src/Stringer.c.obj.d"
+  "libStringer_lib.a"
+  "libStringer_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Stringer_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Data-Base/libs/Stringer/build/CMakeFiles/Stringer_lib.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libStringer_lib.a"
+)
