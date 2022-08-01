397532db5f59152ce9eee736bea73f9cb4bf1678
Mohamed Ashraf <68484397+mohamedashraf-eng@users.noreply.github.com>
2022-08-01 20:03:42 +0200

    Stack things

---
+++ Data-Base/libs/Stack/build/CMakeFiles/Stack_lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Stack_lib.dir/src/Stack.c.obj"
+  "CMakeFiles/Stack_lib.dir/src/Stack.c.obj.d"
+  "libStack_lib.a"
+  "libStack_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Stack_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
