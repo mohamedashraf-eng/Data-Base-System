1d3e40243fe1763bf61ce0a67c4a3d74a84fc76f
Mohamed Ashraf <68484397+mohamedashraf-eng@users.noreply.github.com>
2022-08-01 20:10:41 +0200

    main

--- Data-Base/main.c
+++ Data-Base/main.c
@@ -1,1 +1,29 @@
+/**
+ * @file main.c
+ * @author Mohamed Wx (mohamedashrafwx@gmail.com)
+ * @brief main execute unit for the program.
+ * @version 0.1
+ * @date 2022-08-01
+ * 
+ * @copyright Copyright (c) Wx Software 2022
+ * 
+ */
 
+#include "Global.h"
+
+
+/**
+ * @brief main entry point for the program.
+ * 
+ * @return int 
+ */
+int main(void)
+{
+
+
+
+    printf("\n Hello Project! \n");
+
+
+    return 0;
+}
