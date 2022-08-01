cfc9bb29cdea89ba66ce9f9f8695f76c3e60986a
Mohamed Ashraf <68484397+mohamedashraf-eng@users.noreply.github.com>
2022-08-01 20:00:53 +0200

    Logs things

---
+++ Data-Base/libs/Logs/build/CMakeFiles/Logs_lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Logs_lib.dir/src/Logs.c.obj"
+  "CMakeFiles/Logs_lib.dir/src/Logs.c.obj.d"
+  "libLogs_lib.a"
+  "libLogs_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Logs_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Data-Base/libs/Logs/build/CMakeFiles/Logs_lib.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libLogs_lib.a"
+)
